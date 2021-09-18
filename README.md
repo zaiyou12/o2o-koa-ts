60e8361c3f7145541407fe16e9094c27d93b5bb0
Aaron So <zaiyou12@gmail.com>
2021-09-18 16:18:54 +0900

    ➕ Add helmet and cors

---
+++ README.md
@@ -0,0 +1,13 @@
+# O2O koa ts
+
+```sh
+sh mariadb.sh
+
+docker inspect CONTAINER_NAME
+
+mysql -u root -p
+```
+
+```sql
+CREATE DATABASE o2o
+```
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Koa from "koa";
+import "reflect-metadata";
+import cors from "@koa/cors";
+import helmet from "koa-helmet";
 import bodyParser from "koa-bodyparser";
-import { createConnection, ConnectionOptions } from "typeorm"
-import "reflect-metadata"
+import { createConnection, ConnectionOptions } from "typeorm";
 
-import { config } from "./config"
-import { unprotectedRouter } from "./router/unprotectedRoutes"
+import { config } from "./config";
+import { unprotectedRouter } from "./router/unprotectedRoutes";
 
 const connectionOptions: ConnectionOptions = {
   type: "mariadb",
@@ -13,17 +15,46 @@ const connectionOptions: ConnectionOptions = {
   logging: false,
   entities: config.dbEntitiesPath,
   ssl: config.dbsslconn,
-  extra: {}
-}
+  extra: {},
+};
 
-createConnection(connectionOptions).then(async () => {
-  const app = new Koa()
-  // Enable bodyParser with default options
-  app.use(bodyParser())
-  // these routes are NOT protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
-  app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods())
-  
-  app.listen(config.port, ()=> {
-    console.log(`Koa server is listeng on port ${config.port}`)
+createConnection(connectionOptions)
+  .then(async () => {
+    const app = new Koa();
+
+    // Provides important security headers to make your app more secure
+    app.use(
+      helmet.contentSecurityPolicy({
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'", "cdnjs.cloudflare.com"],
+          styleSrc: [
+            "'self'",
+            "'unsafe-inline'",
+            "cdnjs.cloudflare.com",
+            "fonts.googleapis.com",
+          ],
+          fontSrc: ["'self'", "fonts.gstatic.com"],
+          imgSrc: [
+            "'self'",
+            "data:",
+            "online.swagger.io",
+            "validator.swagger.io",
+          ],
+        },
+      })
+    );
+
+    // Enable cors with default options
+    app.use(cors());
+
+    // Enable bodyParser with default options
+    app.use(bodyParser());
+    // these routes are NOT protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
+    app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods());
+
+    app.listen(config.port, () => {
+      console.log(`Koa server is listeng on port ${config.port}`);
+    });
   })
-}).catch((error: string) => console.log("TypeORM connection error: ", error))
+  .catch((error: string) => console.log("TypeORM connection error: ", error));
