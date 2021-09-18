e8da4770bb1642da785b9216780774ac3999f39f
Aaron So <zaiyou12@gmail.com>
2021-09-18 19:22:38 +0900

    ✨ Add many to many table

--- src/entity/employee.ts
+++ src/entity/employee.ts
@@ -1,23 +1,50 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
-import { Length, IsEmail } from "class-validator"
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  UpdateDateColumn,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Length, IsEmail } from "class-validator";
+import { Employer } from "./employer";
 
 @Entity()
 export class Employee {
   @PrimaryGeneratedColumn()
-  id: number
+  id: number;
 
   @Column({ length: 80 })
   @Length(10, 80)
-  name: string
+  name: string;
 
   @Column({ length: 100 })
   @Length(10, 100)
   @IsEmail()
-  email: string
+  email: string;
+
+  @ManyToMany((type) => Employer)
+  @JoinTable({
+    name: "employee_employer",
+    joinColumn: {
+      name: "employee_id",
+    },
+    inverseJoinColumn: {
+      name: "employer_id",
+    },
+  })
+  employers: Employee[];
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
 }
 
 export const userSchema = {
   id: { type: "number", required: true, example: 1 },
   name: { type: "string", required: true, example: "Aaron" },
-  email: { type: "string", required: true, example: "aaron.so@gmail.com" }
-}
+  email: { type: "string", required: true, example: "aaron.so@gmail.com" },
+};
--- src/entity/employer.ts
+++ src/entity/employer.ts
@@ -1,19 +1,31 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import {
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 
 @Entity()
 export class Employer {
   @PrimaryGeneratedColumn()
-  id: number
+  id: number;
 
   @Column({ length: 80 })
-  name: string
+  name: string;
 
   @Column({ length: 100 })
-  email: string
+  email: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
 }
 
 export const userSchema = {
   id: { type: "number", required: true, example: 1 },
   name: { type: "string", required: true, example: "Aaron" },
-  email: { type: "string", required: true, example: "aaron.so@gmail.com" }
-}
+  email: { type: "string", required: true, example: "aaron.so@gmail.com" },
+};
