7b73da3b2207f96734475d9e6647a4991ed44a8a
Aaron So <zaiyou12@gmail.com>
2021-09-18 16:32:11 +0900

    ➕ Add winston as logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Koa from "koa";
 import "reflect-metadata";
 import cors from "@koa/cors";
+import winston from "winston";
 import helmet from "koa-helmet";
 import bodyParser from "koa-bodyparser";
 import { createConnection, ConnectionOptions } from "typeorm";
 
+import { logger } from "./logger";
 import { config } from "./config";
 import { unprotectedRouter } from "./router/unprotectedRoutes";
 
@@ -47,6 +49,9 @@ createConnection(connectionOptions)
 
     // Enable cors with default options
     app.use(cors());
+
+    // Logger middleware -> use winston as logger (logger.ts with config)
+    app.use(logger(winston))
 
     // Enable bodyParser with default options
     app.use(bodyParser());
